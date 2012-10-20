1c8da1078dd8ac17e1cbbc76a3fe02b33b976cfe
Eduardo Bortoluzzi Jr <sombrabr@gmail.com>
2012-10-20 07:52:03 -0700

    Homework 2 - Part B — RottenPotatoes Enhancement #1: Sorting the list of all movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all(:order => params[:sort])
+    instance_variable_set("@#{params[:sort]}_hilite", 'hilite')
   end
 
   def new
