3c33987a5e7e591a69c24ad79ee6013bf8fe472d
Eduardo Bortoluzzi Jr <sombrabr@gmail.com>
2012-10-21 07:52:11 -0700

    Homework 2 - ROTTENPOTATOES ENHANCEMENT #3: REMEMBER THE SETTINGS

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,21 +4,26 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+
+    @saved_sort = session[:sort]
+    @saved_ratings = session[:ratings]
   end
 
   def index
     if params[:ratings].nil?
-        keys = Movie.get_ratings(nil).map do |r|
-            r[:rating]
-        end
-    else
-        keys = params[:ratings].keys
+        params[:ratings] = Hash[*Movie.get_ratings(nil).select { |r| r[:checked] }.map { |r| [ r[:rating], '1' ] }.flatten ]
     end
+
+    keys = params[:ratings].keys
 
     @movies = Movie.all(:order => params[:sort], :conditions => { :rating => keys })
     instance_variable_set("@#{params[:sort]}_hilite", 'hilite')
 
     @all_ratings = Movie.get_ratings(keys)
+
+    session[:sort] = params[:sort]
+    session[:ratings] = params[:ratings]
+    
   end
 
   def new
